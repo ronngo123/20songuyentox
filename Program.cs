71bce0e6f221dbec57114e156ca5fdabf1e49274
ronngo <accronne2@gmail.com>
2020-09-10 21:11:13 +0700

    20songuyento

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+namespace _20songuyentodautien
+{
+    class Program
+    {
+        public static void snt20(){
+            int index = 0;
+            for (int i = 2; i < 200; i++)
+            {
+                bool check = true;
+                for (int count = 2; count <= Math.Sqrt(i); count++)
+                {
+                    if (i % count == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    index = index + 1;
+                    if (index <= 20)
+                    {
+                        Console.WriteLine(i + "\t");
+                    }
+                }
+            }
+          
+
+        }
+        static void Main(string[] args)
+        {
+         
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("20 số nguyên tố đầu tiên là:");
+        snt20();
+
+        }
+    }
+}
